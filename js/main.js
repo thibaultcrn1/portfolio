f5548cc085851bcf2c6469bd464d174acf12afe3
thibaultcrn1 <tcarron6@outlook.com>
2020-10-17 23:34:09 +0200

    End project.

--- js/main.js
+++ js/main.js
@@ -16,4 +16,42 @@ $(document).ready(function() {
             }
         );
     });
+
+    // Sent mail with ajax
+    $('#send_email').click(function(e){
+		e.preventDefault();
+		var data = {
+			email: $('#email').val(),
+			name: $('#name').val(),
+			objet: $('#firstname').val(),
+			message: $('#message').val()
+        };
+        
+        // AJAX
+		$.ajax({
+			url: "mail.php",
+			type: 'POST',
+			data: data,
+			success: function(data) {
+				$('#js_alert_success').css({'display' : 'block'});
+				setTimeout(function(){
+					$('#js_alert_success').css({'display' : 'none'});
+					$('#email').val("");
+					$('#name').val("");
+					$('#firstname').val("");
+					$('#message').val("")
+				}, 3000);
+			},
+			error: function(data) {
+				$('#js_alert_danger').css({'display' : 'block'});
+				setTimeout(function(){
+					$('#js_alert_danger').css({'display' : 'none'});
+					$('#email').val("");
+					$('#name').val("");
+					$('#firstname').val("");
+					$('#message').val("")
+				}, 3000);
+			}
+		});
+	});
 });
